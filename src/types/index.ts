4c4873974ef78b2e535c3f47372fec168156a187
rabbaniha <hrabbaniha@gmail.com>
2024-03-07 19:50:35 +0330

    create user type

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,7 +1,25 @@
+import React from "react";
+
 export type InewUser = {
-  firstName: string;
-  lastName: string;
+  name: string;
   userName: string;
   email: string;
   password: string;
+};
+
+export type IUser = {
+  id: string;
+  name: string;
+  userName: string;
+  email: string;
+  imageUrl: string;
+  bio: string;
+};
+export type Icontext = {
+  user: IUser;
+  isLoading: boolean;
+  setUser: React.Dispatch<React.SetStateAction<IUser>>;
+  isAuthenticate: boolean;
+  setIsAuthenticate: React.Dispatch<React.SetStateAction<boolean>>;
+  checkAuthUser: () => Promise<boolean>;
 };

539cf2d7b267351db0d18f7bfedbfba9a4faab2f
rabbaniha <hrabbaniha@gmail.com>
2024-03-07 14:55:41 +0330

    add shadcn to root

--- src/App.tsx
+++ src/App.tsx
@@ -2,17 +2,21 @@ import { Route, Routes } from "react-router-dom";
 import AuthLayout from "./Auth/AuthLayout";
 import SignIn from "./Auth/forms/SignIn";
 import SignUp from "./Auth/forms/SignUp";
-import "./App.css"
+import { Toaster } from "@/components/ui/toaster";
+import "./App.css";
 
 const App = () => {
   return (
-    <Routes>
-      {/* public */}
-      <Route element={<AuthLayout />}>
-        <Route path="/sign-in" element={<SignIn />} />
-        <Route path="/sign-up" element={<SignUp />} />
-      </Route>
-    </Routes>
+    <main className="flex h-screen">
+      <Routes>
+        {/* public */}
+        <Route element={<AuthLayout />}>
+          <Route path="/sign-in" element={<SignIn />} />
+          <Route path="/sign-up" element={<SignUp />} />
+        </Route>
+      </Routes>
+      <Toaster />
+    </main>
   );
 };
 

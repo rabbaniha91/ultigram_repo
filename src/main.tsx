88678d3121196759bfb202b8491f2f9e159749ed
rabbaniha <hrabbaniha@gmail.com>
2024-03-07 19:49:47 +0330

    hold context provider and query provider top on all components

--- src/main.tsx
+++ src/main.tsx
@@ -2,11 +2,17 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import { BrowserRouter } from "react-router-dom";
+import AuthContextProvider from "./context/AuthContext.tsx";
+import QueryProvider from "./lib/react-query/QueryProvider.tsx";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <QueryProvider>
+        <AuthContextProvider>
+          <App />
+        </AuthContextProvider>
+      </QueryProvider>
     </BrowserRouter>
   </React.StrictMode>
 );

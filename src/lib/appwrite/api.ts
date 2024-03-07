e561d91e85216d179f67dff7e325a9f3c83a9e9b
rabbaniha <hrabbaniha@gmail.com>
2024-03-07 14:32:18 +0330

    create a signup api and save new user to db api

--- src/lib/appwrite/api.ts
+++ src/lib/appwrite/api.ts
@@ -1,15 +1,31 @@
 import { ID } from "appwrite";
 import { InewUser } from "@/types";
-import { account } from "./config";
+import { account, appwriteConfig, avatars, databases } from "./config";
+import App from "@/App";
+
+// user Authenticate
 
 export const createNewAccount = async (user: InewUser) => {
   try {
-    const newUser = await account.create(
+    const newAccount = await account.create(
       ID.unique(),
-      user.userName,
       user.email,
-      user.password
+      user.password,
+      user.userName
     );
+
+    if (!newAccount) throw Error;
+
+    const avatarUrl = avatars.getInitials(user.userName);
+
+    const newUser = await saveUserToDB({
+      accountId: newAccount.$id,
+      firstName: user.firstName,
+      lastName: user.lastName,
+      userName: newAccount.name,
+      email: newAccount.email,
+      imageUrl: avatarUrl,
+    });
 
     return newUser;
   } catch (error) {
@@ -17,3 +33,28 @@ export const createNewAccount = async (user: InewUser) => {
     return error;
   }
 };
+
+// save new user to db
+
+export async function saveUserToDB(user: {
+  accountId: string;
+  firstName: string;
+  lastName: string;
+  userName: string;
+  email: string;
+  imageUrl: URL;
+}) {
+  try {
+    const newUser = await databases.createDocument(
+      appwriteConfig.databaseId,
+      appwriteConfig.userCollectionId,
+      ID.unique(),
+      user
+    );
+
+    return newUser;
+  } catch (error) {
+    console.log(error);
+    return error;
+  }
+}

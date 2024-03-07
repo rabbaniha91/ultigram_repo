ae730d50fb4cbe2d95e5c830cc31247fe32ce782
rabbaniha <hrabbaniha@gmail.com>
2024-03-07 19:50:05 +0330

    fixed some issues

--- src/lib/validation/index.ts
+++ src/lib/validation/index.ts
@@ -1,8 +1,7 @@
 import * as z from "zod"
 
 export const signUpValidateSchema = z.object({
-    firstName: z.string().min(3, {message: "Too short!"}),
-    lastName: z.string().min(3, {message: "Too short!"}),
+    name: z.string().min(3, {message: "Too Short!"}),
     userName: z.string().min(2, {message: "Too short!"}),
     email: z.string().email({message: "Please enter the valid email address"}),
     password: z.string()

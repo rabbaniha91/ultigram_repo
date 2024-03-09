e3d81e68b69fc10804f683859749c34b4c36ccb4
rabbaniha <hrabbaniha@gmail.com>
2024-03-09 12:31:19 +0330

    implement RootLayout

--- src/root/RootLayout.tsx
+++ src/root/RootLayout.tsx
@@ -1,6 +1,19 @@
+import { Outlet } from "react-router-dom"
+import SideBar from "@/components/shared/SideBar"
+import TopBar from "@/components/shared/TopBar"
+import BottomBar from "@/components/shared/BottomBar"
+
 const RootLayout = () => {
   return (
-    <div>RootLayout</div>
+    <div className="w-full md:flex">
+        <TopBar />
+        <SideBar />
+        <section className="flex flex-1 h-full">
+            <Outlet />
+        </section>
+
+    <BottomBar />
+    </div>
   )
 }
 

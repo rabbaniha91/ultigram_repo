9e93d6e3383873b00f14f1f6050ccef2ea8cce13
rabbaniha <hrabbaniha@gmail.com>
2024-03-09 10:45:37 +0330

    create validation schema for sign in form

--- src/lib/validation/index.ts
+++ src/lib/validation/index.ts
@@ -1,8 +1,12 @@
-import * as z from "zod"
+import * as z from "zod";
 
 export const signUpValidateSchema = z.object({
-    name: z.string().min(3, {message: "Too Short!"}),
-    userName: z.string().min(2, {message: "Too short!"}),
-    email: z.string().email({message: "Please enter the valid email address"}),
-    password: z.string()
-  });
+  name: z.string().min(3, { message: "Too Short!" }),
+  userName: z.string().min(2, { message: "Too short!" }),
+  email: z.string().email({ message: "Please enter the valid email address" }),
+  password: z.string(),
+});
+export const signInValidateSchema = z.object({
+  email: z.string().email({ message: "Please enter the valid email address" }),
+  password: z.string(),
+});

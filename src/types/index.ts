81a396c76871c9f4d5aee380881399a020223522
rabbaniha <hrabbaniha@gmail.com>
2024-03-09 12:31:28 +0330

    create some types

--- src/types/index.ts
+++ src/types/index.ts
@@ -23,3 +23,36 @@ export type Icontext = {
   setIsAuthenticate: React.Dispatch<React.SetStateAction<boolean>>;
   checkAuthUser: () => Promise<boolean>;
 };
+
+export type INavLink = {
+  imgURL: string;
+  route: string;
+  label: string;
+};
+
+export type IUpdateUser = {
+  userId: string;
+  name: string;
+  bio: string;
+  imageId: string;
+  imageUrl: URL | string;
+  file: File[];
+};
+
+export type INewPost = {
+  userId: string;
+  caption: string;
+  file: File[];
+  location?: string;
+  tags?: string;
+};
+
+export type IUpdatePost = {
+  postId: string;
+  caption: string;
+  imageId: string;
+  imageUrl: URL;
+  file: File[];
+  location?: string;
+  tags?: string;
+};

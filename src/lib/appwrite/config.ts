9cf655678d1fb8e25ba60054260c2c5e0c131f46
rabbaniha <hrabbaniha@gmail.com>
2024-03-09 09:59:04 +0330

    fixed issues

--- src/lib/appwrite/config.ts
+++ src/lib/appwrite/config.ts
@@ -5,7 +5,7 @@ export const appwriteConfig = {
   url: import.meta.env.VITE_APPWRITE_URL,
   storageId: import.meta.env.VITE_APPWRITE_STORAGE_ID,
   databaseId: import.meta.env.VITE_APPWRITE_DATABASE_ID,
-  userCollectionId: import.meta.env.VITE_APPWRITE_SAVES_COLLECTION,
+  userCollectionId: import.meta.env.VITE_APPWRITE_USERS_COLLECTION,
   postCollectionId: import.meta.env.VITE_APPWRITE_POSTS_COLLECTION,
   saveCollectionId: import.meta.env.VITE_APPWRITE_SAVES_COLLECTION,
 };

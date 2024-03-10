08acf83a9493fbc9020fa8b3f13e466060f39bf5
rabbaniha <hrabbaniha@gmail.com>
2024-03-10 09:42:11 +0330

    implement Home page

--- src/root/pages/Home.tsx
+++ src/root/pages/Home.tsx
@@ -1,7 +1,24 @@
-const Home = () => {
-  return (
-    <div>Home</div>
-  )
-}
+import Loader from "@/components/shared/Loader";
 
-export default Home
+const Home = () => {
+  const isLoading = true;
+  const posts = null;
+  return (
+    <div className="flex flex-1">
+      <div className="home-container">
+        <div className="home-posts">
+          <h2 className="h3-bold md:h2-bold text-left w-full">Home Feed</h2>
+          {isLoading && !posts ? (
+            <div className="flex-center h-auto">  
+              <Loader size="size-20"/>
+            </div>
+          ) : (
+            <></>
+          )}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Home;

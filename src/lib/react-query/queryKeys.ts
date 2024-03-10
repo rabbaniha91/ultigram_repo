fd2f3f4abac1aa36dd5aa007205b1618a72cb563
rabbaniha <hrabbaniha@gmail.com>
2024-03-10 16:33:18 +0330

    add keys

--- src/lib/react-query/queryKeys.ts
+++ src/lib/react-query/queryKeys.ts
@@ -1,3 +1,8 @@
 export enum QUERY_KEYS {
-    GET_RECENT_POST = 'getRecentPosts'
+    GET_RECENT_POST = 'getRecentPosts',
+    GET_POST_BY_ID = "getPostById",
+    GET_RECENT_POSTS = 'getrecentPosts',
+    GET_POSTS = 'getPosts',
+    GET_CURRENT_USER = 'getCurrentUser'
+
 }

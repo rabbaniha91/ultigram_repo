73511974461d8a1db44c5c3bccd626a8a2af2da9
rabbaniha <hrabbaniha@gmail.com>
2024-03-10 09:36:43 +0330

    get size from prop

--- src/components/shared/Loader.tsx
+++ src/components/shared/Loader.tsx
@@ -1,8 +1,8 @@
-const Loader = () => {
+const Loader = ({size} : {size: string}) => {
   return (
     <div className="flex-center gap-2">
-      <img src="/assets/icons/my-loader.svg" alt="loader" className="size-10"/>
-      <span>Loading ...</span>
+      <img src="/assets/icons/my-loader.svg" alt="loader" className={size}/>
+      {/* <span>Loading ...</span> */}
     </div>
   );
 };

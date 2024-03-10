234299914c7b4cb771f5823a6acbabf62067a355
rabbaniha <hrabbaniha@gmail.com>
2024-03-10 16:31:39 +0330

    add useMutate for likePost, savePost, deleteSavePost

--- src/lib/react-query/queries.ts
+++ src/lib/react-query/queries.ts
@@ -8,6 +8,11 @@ import {
 import {
   createNewAccount,
   createPost,
+  deleteSavedPost,
+  getCurrentUser,
+  getRecentPost,
+  likePost,
+  savePost,
   signInAccount,
   signoutAccount,
 } from "../appwrite/api";
@@ -43,5 +48,96 @@ export const useCreatePost = () => {
         queryKey: [QUERY_KEYS.GET_RECENT_POST],
       });
     },
+  });
+};
+
+// get posts
+
+export const useGetRecentPosts = () => {
+  return useQuery({
+    queryKey: [QUERY_KEYS.GET_RECENT_POST],
+    queryFn: getRecentPost,
+  });
+};
+
+// like post
+
+export const useLikePost = () => {
+  const queryClient = useQueryClient();
+
+  return useMutation({
+    mutationFn: ({
+      postId,
+      likeArray,
+    }: {
+      postId: string;
+      likeArray: string[];
+    }) => likePost(postId, likeArray),
+    onSuccess: (data) => {
+      queryClient.invalidateQueries({
+        queryKey: [QUERY_KEYS.GET_POST_BY_ID, data?.$id],
+      });
+      queryClient.invalidateQueries({
+        queryKey: [QUERY_KEYS.GET_RECENT_POSTS],
+      });
+      queryClient.invalidateQueries({
+        queryKey: [QUERY_KEYS.GET_POSTS],
+      });
+      queryClient.invalidateQueries({
+        queryKey: [QUERY_KEYS.GET_CURRENT_USER],
+      });
+    },
+  });
+};
+
+// save post
+
+export const useSavePost = () => {
+  const queryClient = useQueryClient();
+
+  return useMutation({
+    mutationFn: ({ postId, userId }: { postId: string; userId: string }) =>
+      savePost(postId, userId),
+    onSuccess: () => {
+      queryClient.invalidateQueries({
+        queryKey: [QUERY_KEYS.GET_RECENT_POSTS],
+      });
+      queryClient.invalidateQueries({
+        queryKey: [QUERY_KEYS.GET_POSTS],
+      });
+      queryClient.invalidateQueries({
+        queryKey: [QUERY_KEYS.GET_CURRENT_USER],
+      });
+    },
+  });
+};
+
+// delete saved post
+
+export const useDeleteSavePost = () => {
+  const queryClient = useQueryClient();
+
+  return useMutation({
+    mutationFn: (savedRecordId: string) => deleteSavedPost(savedRecordId),
+    onSuccess: () => {
+      queryClient.invalidateQueries({
+        queryKey: [QUERY_KEYS.GET_RECENT_POSTS],
+      });
+      queryClient.invalidateQueries({
+        queryKey: [QUERY_KEYS.GET_POSTS],
+      });
+      queryClient.invalidateQueries({
+        queryKey: [QUERY_KEYS.GET_CURRENT_USER],
+      });
+    },
+  });
+};
+
+// get current user query
+
+export const useGetCurrentUser = () => {
+  return useQuery({
+    queryKey: [QUERY_KEYS.GET_CURRENT_USER],
+    queryFn: getCurrentUser,
   });
 };
